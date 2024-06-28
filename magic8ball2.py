831e085f89940156b079164604a6883ff085f6ea
MarcieLee <37553625+rmartin127@users.noreply.github.com>
2024-06-27 20:41:32 -0400

    Delete magic8ball2.py

--- magic8ball2.py
+++
@@ -1,5 +0,0 @@
-# Write your code here :-)
-import random
-messages = ['It is certain','It is decidedly so','Yes, Definitely', 'Reply hazy, try again', \
-'Ask again later', 'Concentrate and ask again', 'My reply is no', 'Outlook not so good', 'Very doubtful']
-print(messages[random.randint(0,len(messages) - 1)])
