40da5ee81feae723db4a865cfc3d6d2fb5ce2617
MarcieLee <37553625+rmartin127@users.noreply.github.com>
2024-06-27 20:39:29 -0400

    Delete goodbye.py

--- goodbye.py
+++
@@ -1,9 +0,0 @@
-# Write your code here :-)
-import sys
-
-while True:
-    print('Type exit to exit.')
-    response = input()
-    if response == 'exit':
-        sys.exit()
-    print('You typed ' +  response + '.')
