043b7348f206c17b90dda3f79fbdf4f1620cb9d1
MarcieLee <37553625+rmartin127@users.noreply.github.com>
2024-06-27 20:45:13 -0400

    Delete yourName2.py

--- yourName2.py
+++
@@ -1,8 +0,0 @@
-# Write your code here :-)
-name = ''
-while True:
-    print('Please type your name.')
-    name = input()
-    if name == 'your name':
-        break
-print('Thank You ' + name + '!')
