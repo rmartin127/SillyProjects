27c5fb563cd295e8b3369a7ccc7b9297d3680566
MarcieLee <37553625+rmartin127@users.noreply.github.com>
2024-07-11 22:32:35 -0400

    Update to factors.py
    Updated factors.py to now efficiently list all factor pairs

--- Python/factors.py
+++ Python/factors.py
@@ -1,23 +1,30 @@
 # Write your code here :-)
 import math
+
 num = input("Factorize a number:")
 try:
     num = int(num)
-    half = math.ceil(num/2)
+    half = math.ceil(num / 2)
     count = 2
-    factors = [num,1]
-    for i in range(2,half+1):
+    factors = [num, 1]
+    factor_pair = [(1, num)]
+    pair_count = 1
+    new_pair = [1,2]
+    for i in range(2, half + 1):
         if num % i == 0:
             count += 1
             factors.append(i)
+            if i not in factor_pair:
+                factor_pair.append([i, (num // i)])
+    factor_half = math.ceil(len(factor_pair) / 2)
+    for i in range(0,factor_half):
+        print(i)
+        new_pair[i] = factor_pair[i]
     if count == 2:
-        print(f'{num} is prime')
-
+        print(f"{num} is prime")
     else:
-        print(f'There are {count} factors of {num}, they are: \n{sorted(factors)}')
+        print(
+            f"There are {count} factors of {num}, they are: \n{sorted(factors)} \n{new_pair}"
+        )
 except:
     print("Not a number")
-#try:
-
-#except:
-#    print(f"unexpted {type(num)} in bagging area")
